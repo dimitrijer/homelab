bb7e4067e3c05fc902f9b04bd9598c40d2271ebc
Dimitrije Radojević <me@dimitrije.website>
2023-12-21 17:56:23 +0100

    Use bundled nix ipxe script instead of writing our own.

--- default.nix
+++ default.nix
@@ -1,47 +1,52 @@
-{ pkgs ? import ./nix/default.nix { }, cmdline ? "" }:
+{ pkgs ? import ./nix/default.nix { } }:
 
 let
   dest = "10.1.100.1";
   as = "admin";
-  netbootIpxe = pkgs.writeText "netboot.ipxe" ''
-    #!ipxe
-    chain --replace --autofree http://${dest}/mac/''${mac:hexhyp}/ipxe
-  '';
   mkNetbuild = { mac, configuration }:
     let
       sys =
         pkgs.nixos
           ({ config, pkgs, lib, modulesPath, ... }: {
             imports = [
-              (modulesPath + "/installer/netboot/netboot-minimal.nix")
+              (modulesPath + "/installer/netboot/netboot.nix")
+              # Allow "nixos-rebuild" to work properly by providing
+              # /etc/nixos/configuration.nix.
+              (modulesPath + "/profiles/clone-config.nix")
+              (modulesPath + "/profiles/minimal.nix")
             ];
             config = import configuration { inherit config; };
           });
       build = sys.config.system.build;
-      ipxeScript = ''
-        #!ipxe
-        # Use the cmdline variable to allow the user to specify custom kernel params
-        # when chainloading this script from other iPXE scripts like netboot.xyz
-        kernel bzImage init=${build.toplevel}/init initrd=initrd nohibernate loglevel=4 ${cmdline}
-        initrd initrd
-        boot
-      '';
 
+      netbuild =
+        pkgs.stdenv.mkDerivation
+          {
+            name = "netbuild-${mac}";
+            unpackPhase = "true";
+
+            installPhase = ''
+              mkdir -p $out/mac/${mac}
+              cp ${build.kernel}/bzImage $out/mac/${mac}/bzImage
+              cp ${build.netbootRamdisk}/initrd $out/mac/${mac}/initrd
+              cp ${build.netbootIpxeScript}/netboot.ipxe $out/mac/${mac}/ipxe
+            '';
+          };
     in
-    pkgs.stdenv.mkDerivation
-      {
-        name = "netbuild-${mac}";
-        unpackPhase = "true";
-
-        ipxe = pkgs.writeText "netboot.ipxe" ipxeScript;
-
-        installPhase = ''
-          mkdir -p $out/mac/${mac}
-          cp ${build.kernel}/bzImage $out/mac/${mac}/bzImage
-          cp ${build.netbootRamdisk}/initrd $out/mac/${mac}/initrd
-          cp $ipxe $out/mac/${mac}/ipxe
+    {
+      inherit netbuild;
+      deploy =
+        pkgs.writeScriptBin "deploy" ''
+          #!${pkgs.runtimeShell}
+          if [ $# -ne 1 ]; then
+            echo "Supply path to private key as first argument"
+            exit 1
+          fi
+          ${pkgs.openssh}/bin/scp -i "$1" -r ${netbuild}/mac ${as}@${dest}:/usb1-part1/ipxe/
         '';
-      };
+    };
+in
+rec {
   alpha = mkNetbuild {
     mac = "48-4d-7e-ee-44-9b";
     configuration = ./alpha.nix;
@@ -58,22 +63,6 @@ let
     mac = "48-4d-7e-ee-48-0d";
     configuration = ./delta.nix;
   };
-
-in
-{
-  deploy =
-    (pkgs.writeScriptBin "deploy" ''
-      #!${pkgs.runtimeShell}
-      if [ $? -ne 1 ]; then
-        echo "Supply path to private key as first argument"
-        exit 1
-      end
-      ${pkgs.openssh}/bin/scp -i "$1" -r ${alpha}/mac ${as}@${dest}:/usb1-part1/ipxe/
-      ${pkgs.openssh}/bin/scp -i "$1" -r ${beta}/mac  ${as}@${dest}:/usb1-part1/ipxe/
-      ${pkgs.openssh}/bin/scp -i "$1" -r ${gamma}/mac ${as}@${dest}:/usb1-part1/ipxe/
-      ${pkgs.openssh}/bin/scp -i "$1" -r ${delta}/mac ${as}@${dest}:/usb1-part1/ipxe/
-      ${pkgs.openssh}/bin/scp -i "$1" ${netbootIpxe} ${as}@${dest}:/usb1-part1/netboot.ipxe
-    '');
 
   nginxIpxe = import ./nginx.nix
     {
