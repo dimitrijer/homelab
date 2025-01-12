9ca724950dd1aca2aa5c3945d575f169547d0d48
Dimitrije Radojević <me@dimitrije.website>
2025-01-12 19:42:55 +0000

    Add skiff pubkey to authorized keys.

--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -15,6 +15,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJklWVMXaRPHb2+px018aQdEldAtzt9+MZHqImMmDFZa dimitrije@prospect"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOHY5AGvFXTnIT2XOxN41dwYFwumLN9+FJdgg03i8IQg dimitrije@endurance"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN7gdT/00QarOYM33vpSOu/KxCUoj8WuLkB+1QFAJ1+Y dimitrije@mackinaw"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKi8BBpDsh9SDfVhfuvlyF2dPYkxtiwWZzQ64ibkmcE dimitrije@skiff"
       ];
     };
   users.users.root.initialHashedPassword = "$6$OZ8xMsNhdISbKv8P$mj2ZqKDCBoxy59H/XphKKnQu3yxIHgwwI9hP3CJdIZHWzRh0boT9dfWArJjoIxAUsMpdxtybdKSr0X01Kclf81";
