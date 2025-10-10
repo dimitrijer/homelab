6d0be5fbe453d217a840ad5580a58a4d2e5626ca
Dimitrije Radojević <me@dimitrije.website>
2025-10-10 04:53:33 +0100

    Bump drbd-utils to latest HEAD, drbd to 9.2.15.

--- nix/overlays/drbd.nix
+++ nix/overlays/drbd.nix
@@ -3,10 +3,10 @@ self: super: {
     (kfinal: kprev: {
       drbd = kprev.drbd.overrideAttrs
         (oldAttrs: rec {
-          version = "9.2.15-rc.1";
+          version = "9.2.15";
           src = super.fetchurl {
             url = "https://pkg.linbit.com/downloads/drbd/9/drbd-${version}.tar.gz";
-            hash = "sha256-67bjS6CK4zrW1suIxMO4na3vFT8Xpzbw095b0/8WhUY=";
+            hash = "sha256-bKaL7wtjlSbUkLRlMSrGYjab0jdS8lu5bgScTbfpllE=";
           };
           makeFlags = oldAttrs.makeFlags ++ [ "CONFIG_DRBD_COMPAT_84=y" ];
         });
@@ -14,9 +14,21 @@ self: super: {
   drbd = super.drbd.overrideAttrs
     (oldAttrs: rec {
       version = "9.32.0";
-      src = super.fetchurl {
-        url = "https://pkg.linbit.com/downloads/drbd/utils/${oldAttrs.pname}-utils-${version}.tar.gz";
-        hash = "sha256-szOM7jSbXEZZ4p1P73W6tK9Put0+wOZar+cUiUNC6M0=";
+      preConfigure = ''
+        ${oldAttrs.preConfigure or ""}
+        ./autogen.sh
+
+        echo -e '#define GITHASH "0"\n#define GITDIFF "0"' > ./user/shared/drbd_buildtag.h
+      '';
+
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ (with super;
+        [ autoconf automake git ]);
+
+      src = super.fetchgit {
+        url = "https://github.com/LINBIT/drbd-utils.git";
+        rev = "fa022500e8401a1004095fd3347325b4a837ccd5";
+        hash = "sha256-+VMu4Oax29IOyxZXjBuxlUrZcxJjBACp0pC1/TGn1fg=";
+        fetchSubmodules = true;
       };
     });
 }
