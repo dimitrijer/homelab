a2f77101763b090c0e0d70ea6e0f838c639519b5
Dimitrije Radojević <me@dimitrije.website>
2026-01-27 19:48:58 +0000

    DRBD9 fixes (drbd 9.3 and latest drbd utils).

--- nix/overlays/drbd.nix
+++ nix/overlays/drbd.nix
@@ -1,35 +1,41 @@
 self: super: {
   linuxPackages = super.linuxPackages.extend
     (kfinal: kprev: {
-      drbd = kprev.drbd.overrideAttrs
+      drbd = let rev = "d3838e6f08d1e56882b8d7265fc9032c2ef8c9cb"; in kprev.drbd.overrideAttrs
         (oldAttrs: rec {
-          version = "9.2.15";
+          version = "9.3.0";
           src = super.fetchgit {
+            inherit rev;
             url = "https://github.com/LINBIT/drbd.git";
-            rev = "d3838e6f08d1e56882b8d7265fc9032c2ef8c9cb";
             hash = "sha256-ijPqeVTBE3bvwKtrHN6NeNg7qx0xBLDqW9dulO/davo=";
           };
+          preConfigure = ''
+            ${oldAttrs.preConfigure or "" } 
+            echo -e 'GIT-hash: ${rev}' > ./drbd/.drbd_git_revision
+          '';
           makeFlags = oldAttrs.makeFlags ++ [ "CONFIG_DRBD_COMPAT_84=y" ];
         });
     });
-  drbd = super.drbd.overrideAttrs
-    (oldAttrs: rec {
-      version = "9.33.0";
-      preConfigure = ''
-        ${oldAttrs.preConfigure or ""}
-        ./autogen.sh
+  drbd =
+    let rev = "2701b04134d81b4e57c14ff2e93ddc355af16523";
+    in super.drbd.overrideAttrs
+      (oldAttrs: rec {
+        version = "9.33.0";
+        preConfigure = ''
+          ${oldAttrs.preConfigure or ""}
+          ./autogen.sh
 
-        echo -e '#define GITHASH "0"\n#define GITDIFF "0"' > ./user/shared/drbd_buildtag.h
-      '';
+          echo -e '#define GITHASH "${rev}"\n#define GITDIFF "0"' > ./user/shared/drbd_buildtag.h
+        '';
 
-      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ (with super;
-        [ autoconf automake git ]);
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ (with super;
+          [ autoconf automake git ]);
 
-      src = super.fetchgit {
-        url = "https://github.com/LINBIT/drbd-utils.git";
-        rev = "abd47fbdf079d96766e4312f52eef038fc2c6723";
-        hash = "sha256-WeQd2q/WgjNpXrTnkegEwVVg9r8wsQmAWbmXgrhoXbY=";
-        fetchSubmodules = true;
-      };
-    });
+        src = super.fetchgit {
+          url = "https://github.com/LINBIT/drbd-utils.git";
+          inherit rev;
+          hash = "sha256-tSICzHtp2IgRNniBCI6OHP9No6lJhDHWvZSdoLL5jbg=";
+          fetchSubmodules = true;
+        };
+      });
 }
