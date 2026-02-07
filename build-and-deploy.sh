ec72b8947aca53db1c754dfc22c692704da7f513
Dimitrije Radojević <me@dimitrije.website>
2026-02-07 13:29:16 +0000

    Add a script to build and deploy all images.

---
+++ build-and-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+IMAGES=(
+    adguard-home
+    audiobookshelf
+    calibre-web
+    ganeti-node
+    jellyfin
+    metrics
+    navidrome
+    paperless
+)
+
+echo "==="
+for image in "${IMAGES[@]}"
+do
+    if [[ -h "$image" ]]; then
+      echo "Removing previous result for $image..."
+      unlink "$image"
+    fi
+    echo "Building $image..."
+    nix-build -I . -A "${image}-http" -o "$image"
+    echo "Deploying $image..."
+    "./$image/bin/deploy" ~/.ssh/id_ed25519
+    echo "==="
+done
+
+echo
+echo "All done!"
