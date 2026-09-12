ab5294c685a56473f47bd079ebffda5ebebd752b
Dimitrije Radojević <me@dimitrije.website>
2026-09-12 11:50:25 +0100

    Do not build ovmf-xen, to shorten build times.

--- nix/overlays/ovmf.nix
+++ nix/overlays/ovmf.nix
@@ -1,20 +1,12 @@
+# UEFI firmware with Secure Boot / TPM / HTTP boot support but *without* the
+# SMM requirement, so it also boots on the i440fx ("pc") machine type.
 self: super: {
-  OVMF = super.OVMF.override {
+  OVMF-nosmm = super.OVMF.override {
     secureBoot = true;
     tpmSupport = true;
     tlsSupport = true;
     httpSupport = true;
     msVarsTemplate = true;
     systemManagementModeRequired = false;
-  };
-
-  # OVMF-xen uses OvmfXen.dsc which doesn't support Secure Boot
-  # and doesn't build EnrollDefaultKeys.efi, so pass an OVMF with
-  # those features disabled.
-  OVMF-xen = super.OVMF-xen.override {
-    OVMF = self.OVMF.override {
-      secureBoot = false;
-      msVarsTemplate = false;
-    };
   };
 }
